75854f84b447037b3f2952c10f9469a0b4b94322
HanQin Cai <44795174+caesarcai@users.noreply.github.com>
2024-06-19 23:22:10 -0400

    Update test_HSNLD.m

--- test_HSNLD.m
+++ test_HSNLD.m
@@ -2,9 +2,9 @@ close all
 clear all
 addpath('PROPACK')
 rng(1000)
-n = 2^16 - 1;             % dimension, n = n1+n2-1
+n = 2^16 - 1;             % dimension, n = n1+n2-1 s.t. n1=n2
 r = 10;                   % rank
-sample_rate = 0.01;       % sample rate
+sample_rate = 0.01;       % 1% sample rate
 m = ceil(sample_rate*n); 
 alpha = 0.1;              % 10% outlier
 k = round(alpha*m);
@@ -22,12 +22,11 @@ mean_of_ox = mean(abs(ox));
 K = randsample(m,k);  % outlier location
 obs(K) = obs(K) + c*mean_of_ox*2*(rand(k,1)-0.5+1i*(rand(k,1)-0.5));
 
-
 gamma_init = max(min(1.5, (1-2*log(n)*r/n)*(1/alpha)),1.2);
 gamma_decay = 0.95;
 tol = 1e-8;
 max_iter = 200;
-proj = true;
+proj = false;
 eta = 0.5;
 
 [x,err,timer] = HSNLD(obs,n,r,M,alpha,eta,gamma_init,gamma_decay,proj,tol,max_iter);
